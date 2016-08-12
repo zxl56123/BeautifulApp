e9ed79836cf02a8e01f44526c549de82698706c0
郑晓龙 <zxl56123@163.com>
2016-08-12 14:32:16 +0800

    代码上传

---
+++ BeautifulApp/BeautifulApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeautifulApp
+//
+//  Created by 郑晓龙 on 16/8/12.
+//  Copyright © 2016年 郑晓龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BeautifulApp/BeautifulApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BeautifulApp
+//
+//  Created by 郑晓龙 on 16/8/12.
+//  Copyright © 2016年 郑晓龙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
